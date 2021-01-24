f6102b89abdeedb2f658e1ee2f42d1c986d54a06
Chapaev Anton <virsus2009@gmail.com>
2021-01-24 13:43:10 +0700

    variant1

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
         System.out.println("Hello world!!!");
         System.out.println("Hello world!!!");
         System.out.println("br5");
-        System.out.println("br10");
+        System.out.println("br10_variant1");
 
     }
 
