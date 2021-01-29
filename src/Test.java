2b9876e5938b96acd122ffacdb97c7c34b3ebe9e
ChapaevAnton <virsus2009@gmail.com>
2021-01-29 11:32:17 +0700

    up two methods main test code

--- src/Test.java
+++ src/Test.java
@@ -5,8 +5,20 @@ public class Test {
         stringSum("456text54");
         stringSum("45654");
 
+        //Вызов статических методов класса ClassInner
+        ClassInner.getPrint();
+        ClassInner.getPrintMain(args);
+
+        // Создадим экземпляр класса ClassInner
+        ClassInner classInner = new ClassInner();
+        classInner.getPrintInner();
+
+        // вызов метода main подкласса ClassInner с передачей параметров
+        ClassInner.main(new String[]{"a", "b", "c"});
+
     }
 
+    //просто метод который сумирует элементы числовой строки
     public static void stringSum(String str) {
         int sum = 0;
         try {
@@ -20,4 +32,30 @@ public class Test {
         }
         System.out.println(sum);
     }
+
+}
+
+//Внутренний класс
+class ClassInner {
+
+    public static void main(String[] args) {
+        getPrint();
+        getPrintMain(args);
+    }
+
+    static void getPrint() {
+        System.out.println("Я внутренний класс ClassInner");
+    }
+
+    static void getPrintMain(String[] str) {
+        System.out.print("Переданные параметры:");
+        for (String parameter : str) {
+            System.out.print(parameter);
+        }
+        System.out.println();
+    }
+    void getPrintInner(){
+        System.out.println("Я экземпляр класса ClassInner");
+    }
+
 }
